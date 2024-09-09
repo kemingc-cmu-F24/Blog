4d3550968d1cf3625cb2ee5cf5ebb056684f0494
Kenny Chen <kemingc@andrew.cmu.edu>
2024-09-09 13:53:53 -0700

    remove console.log in indexController

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -4,7 +4,6 @@ const Project = require('../models/project');
 const getIndex = async (req, res, next) => {
     const recentBlogs = await Blog.find().sort({createdAt: -1}).limit(12).exec();
     const recentProjects = await Project.find().sort({createdAt: -1}).limit(3).exec();
-    console.log(recentProjects);
     return res.render("index", {
         path: "/",
         recentBlogs: recentBlogs,
