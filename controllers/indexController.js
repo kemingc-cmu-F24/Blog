e2d1367eacd06209f14dfefad3b673ada53ee639
Kenny Chen <kemingc@andrew.cmu.edu>
2024-09-08 17:02:18 -0700

    optimize index page layout

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -3,7 +3,7 @@ const Project = require('../models/project');
 
 const getIndex = async (req, res, next) => {
     const recentBlogs = await Blog.find().sort({createdAt: -1}).limit(12).exec();
-    const recentProjects = await Project.find().sort({createdAt: -1}).limit(2).exec();
+    const recentProjects = await Project.find().sort({createdAt: -1}).limit(3).exec();
     console.log(recentProjects);
     return res.render("index", {
         path: "/",
